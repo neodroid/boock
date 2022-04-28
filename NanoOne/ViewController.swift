013feb43961a07a2d708630a3e5af1cbaeb8449e
neodroid <kevinahmad.f@gmail.com>
2022-04-28 13:17:37 +0700

    Initial Commit

---
+++ NanoOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NanoOne
+//
+//  Created by Kevin ahmad on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
